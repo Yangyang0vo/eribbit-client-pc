a595c373e0c415789efac98224c47d10352dc9cb
洋洋 <2372542263@qq.com>
2022-05-12 15:37:40 +0800

    首页主体-左侧分类处理品牌

--- src/views/home/components/home-category.vue
+++ src/views/home/components/home-category.vue
@@ -1,19 +1,50 @@
 <template>
-  <div class="home-category">
+  <div class="home-category" @mouseleave="categoryId = null">
     <ul class="menu">
-      <li v-for="item in menuLIst" :key="item.id">
+      <li :class="{ active: categoryId && categoryId === item.id }" v-for="item in menuLIst" :key="item.id" @mouseenter="categoryId = item.id">
         <RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
         <template v-if="item.children">
           <RouterLink v-for="sub in item.children" :key="sub.id" :to="`/category/sub/${item.id}`">{{ sub.name }}</RouterLink>
         </template>
       </li>
     </ul>
+    <!-- 弹层 -->
+    <div class="layer">
+      <h4>{{ currCategory && currCategory.id === 'brand' ? '分类' : '品牌' }}推荐 <small>根据您的购买或浏览记录推荐</small></h4>
+      <!-- 商品 -->
+      <ul v-if="currCategory && currCategory.goods">
+        <li v-for="item in currCategory.goods" :key="item.id">
+          <RouterLink to="/">
+            <img :src="item.picture" alt="" />
+            <div class="info">
+              <p class="name ellipsis-2">{{ item.name }}</p>
+              <p class="desc ellipsis">{{ item.desc }}</p>
+              <p class="price"><i>¥</i>{{ item.price }}</p>
+            </div>
+          </RouterLink>
+        </li>
+      </ul>
+      <!-- 品牌 -->
+      <ul v-if="currCategory && currCategory.brands">
+        <li class="brand" v-for="brand in currCategory.brands" :key="brand.id">
+          <RouterLink to="/">
+            <img :src="brand.picture" alt="" />
+            <div class="info">
+              <p class="place"><i class="iconfont icon-dingwei"></i>{{ brand.place }}</p>
+              <p class="name ellipsis">{{ brand.name }}</p>
+              <p class="desc ellipsis-2">{{ brand.desc }}</p>
+            </div>
+          </RouterLink>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import { computed, reactive } from 'vue'
+import { computed, reactive, ref } from 'vue'
 import { useStore } from 'vuex'
+import { findBrand } from '@/api/home'
 export default {
   name: 'HomeCategory',
   setup() {
@@ -25,7 +56,9 @@ export default {
     const brand = reactive({
       id: 'brand',
       name: '品牌',
-      children: [{ id: 'brand-children', name: '品牌推荐' }]
+      children: [{ id: 'brand-children', name: '品牌推荐' }],
+      // 品牌列表
+      brands: []
     })
     const menuLIst = computed(() => {
       const list = store.state.category.list.map((item) => {
@@ -38,10 +71,21 @@ export default {
         }
       })
       list.push(brand)
-      console.log(list)
+      // console.log(list)
       return list
     })
-    return { menuLIst }
+
+    // 得到弹出层的推荐商品数据
+    const categoryId = ref(null)
+    const currCategory = computed(() => {
+      return menuLIst.value.find((item) => item.id === categoryId.value)
+    })
+    //  获取品牌数据
+    //  尽量不要在setup前加上async 加上组件渲染就变成异步的了 使用起来比较麻烦
+    findBrand().then((data) => {
+      brand.brands = data.result
+    })
+    return { menuLIst, categoryId, currCategory }
   }
 }
 </script>
@@ -58,7 +102,8 @@ export default {
       padding-left: 40px;
       height: 50px;
       line-height: 50px;
-      &:hover {
+      &:hover,
+      &.active {
         background: @xtxColor;
       }
       a {
@@ -68,6 +113,100 @@ export default {
           font-size: 16px;
         }
       }
+    }
+  }
+  // 弹层样式
+  .layer {
+    width: 990px;
+    height: 500px;
+    background: rgba(255, 255, 255, 0.8);
+    position: absolute;
+    left: 250px;
+    top: 0;
+    display: none;
+    padding: 0 15px;
+    h4 {
+      font-size: 20px;
+      font-weight: normal;
+      line-height: 80px;
+      small {
+        font-size: 16px;
+        color: #666;
+      }
+    }
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+      li {
+        width: 310px;
+        height: 120px;
+        margin-right: 15px;
+        margin-bottom: 15px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        background: #fff;
+
+        &:nth-child(3n) {
+          margin-right: 0;
+        }
+        a {
+          display: flex;
+          width: 100%;
+          height: 100%;
+          align-items: center;
+          padding: 10px;
+          &:hover {
+            background: #e3f9f4;
+          }
+          img {
+            width: 95px;
+            height: 95px;
+          }
+          .info {
+            padding-left: 10px;
+            line-height: 24px;
+            width: 190px;
+            .name {
+              font-size: 16px;
+              color: #666;
+            }
+            .desc {
+              color: #999;
+            }
+            .price {
+              font-size: 22px;
+              color: @priceColor;
+              i {
+                font-size: 16px;
+              }
+            }
+          }
+        }
+      }
+      // 品牌的样式
+      li.brand {
+        height: 180px;
+        a {
+          align-items: flex-start;
+          img {
+            width: 120px;
+            height: 160px;
+          }
+          .info {
+            p {
+              margin-top: 8px;
+            }
+            .place {
+              color: #999;
+            }
+          }
+        }
+      }
+    }
+  }
+  &:hover {
+    .layer {
+      display: block;
     }
   }
 }
